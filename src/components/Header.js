3981e427f465bd08ff7c35d9a7e2d02caecf091d
abindadna <abindadna6397@gmail.com>
2022-06-26 14:41:35 +0700

    challenge 9

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
-import React from 'react';
+import React from "react";
+import style from "./header.module.css";
 
 const Header = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <header className={style.header}>
+      <h1 className={style.header__title}>Call a Friend</h1>
+      <p className={style.header__subtitle}>your friendly contact app </p>
+    </header>
+  );
+};
 
 export default Header;
